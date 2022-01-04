00f048eb7075e2d87100f15dee78cf8af5635d77
webomroz <eziciterminator04@gmail.com>
2022-01-04 23:35:59 +0300

    Add comment jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import ReactDOM from "react-dom";
 
 const App = () => {
     return (
-        <div>Hi there</div>
+        <div className="ui comments">
+
+            <div className="comment">
+                <a className="avatar">
+                    <img alt="Avatar" />
+                </a>
+                <div className="content">
+                    <a className="author">Adil</a>
+                    <div className="metadata">
+                        <span className="date">Today at 5:42PM</span>
+                    </div>
+                    <div className="text">
+                        How artistic!
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     );
 };
 
